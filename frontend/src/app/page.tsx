c2b8f346718c14715fff1ca4920dceac78aa88fc
Leonardo Cintra <leonardo.ncintra@outlook.com>
2024-03-23 09:25:26 -0300

    Intial Project frontend

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,53 @@
+import { HeroSection } from "@/components/custom/hero-section";
+import { flattenAttributes } from "@/lib/utils";
+import qs from "qs";
+
+const homePageQuery = qs.stringify({
+  populate: {
+    blocks: {
+      populate: {
+        image: {
+          fields: ["url", "alternativeText"],
+        },
+        link: {
+          populate: true,
+        },
+      },
+    },
+  },
+});
+
+async function getStrapiData(path: string) {
+  const baseUrl = "http://localhost:1337";
+
+  const url = new URL(path, baseUrl);
+  url.search = homePageQuery;
+
+  console.log(url.href);
+
+  try {
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await response.json();
+    const flattened = flattenAttributes(data);
+    return flattened;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+export default async function Home() {
+  const strapiData = await getStrapiData("/api/home-page");
+  console.dir(strapiData, { depth: null });
+  const { title, description, blocks } = strapiData;
+
+  return (
+    <main className="container mx-auto py-6">
+      <HeroSection data={blocks[0]} />
+    </main>
+  );
+}
