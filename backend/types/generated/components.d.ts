7c9ad8efa179010ca4dab258dc81c6de78e91eae
Leonardo Cintra <leonardo.ncintra@outlook.com>
2024-03-24 21:37:57 -0300

    :sparkles: add global header footer

--- backend/types/generated/components.d.ts
+++ backend/types/generated/components.d.ts
@@ -39,6 +39,31 @@ export interface LayoutFeaturesSections extends Schema.Component {
   };
 }
 
+export interface LayoutFooter extends Schema.Component {
+  collectionName: 'components_layout_footers';
+  info: {
+    displayName: 'Footer';
+    icon: 'underline';
+  };
+  attributes: {
+    logoText: Attribute.Component<'components.link'>;
+    text: Attribute.Text;
+    socialLink: Attribute.Component<'components.link', true>;
+  };
+}
+
+export interface LayoutHeader extends Schema.Component {
+  collectionName: 'components_layout_headers';
+  info: {
+    displayName: 'Header';
+    icon: 'archive';
+  };
+  attributes: {
+    logoText: Attribute.Component<'components.link'>;
+    ctaButton: Attribute.Component<'components.link'>;
+  };
+}
+
 export interface LayoutHeroSection extends Schema.Component {
   collectionName: 'components_layout_hero_sections';
   info: {
@@ -60,6 +85,8 @@ declare module '@strapi/types' {
       'components.feature': ComponentsFeature;
       'components.link': ComponentsLink;
       'layout.features-sections': LayoutFeaturesSections;
+      'layout.footer': LayoutFooter;
+      'layout.header': LayoutHeader;
       'layout.hero-section': LayoutHeroSection;
     }
   }
